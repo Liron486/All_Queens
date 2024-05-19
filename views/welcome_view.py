80ba60c91aab20575cfb133f138426d5119118bd
Liron Tzemach <lirontzemach@gmail.com>
2024-05-19 21:43:51 +0300

    [Fix] adding mouse signals to welcome page

--- views/welcome_view.py
+++ views/welcome_view.py
@@ -7,37 +7,35 @@ class WelcomeWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.controller = None  # Initialize controller as None
-        self.background_label = QLabel(self)  # Create a label to hold the background image
+        self.controller = None
+        self.background_label = QLabel(self) 
         self.init_ui()
 
     def set_controller(self, controller):
         self.controller = controller
 
     def init_ui(self):
-        self.setWindowTitle('Welcome to 4 Queens')
-        
-        # Set up the background image
-        pixmap = QPixmap('resources/images/welcome_background.png')
-        self.background_label.setPixmap(pixmap.scaled(self.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation))
-        self.background_label.setFixedSize(self.size())
-        self.background_label.lower()  # Ensure the label is behind all other widgets
-
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-        layout = QVBoxLayout(central_widget)
-
+        self.setWindowTitle('4 Queens')
+        self.SetBackgroundImage()
         self.showFullScreen()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
-            self.escapePressed.emit()  # Emit the signal
+            self.escapePressed.emit()
         else:
             self.controller.play()
 
+    def mousePressEvent(self, event):
+        self.controller.play()
+
     def resizeEvent(self, event):
-        # Update the background label size and pixmap when the window is resized
         super(WelcomeWindow, self).resizeEvent(event)
         if self.background_label:
             self.background_label.setFixedSize(self.size())
             self.background_label.setPixmap(QPixmap('resources/images/welcome_background.png').scaled(self.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation))
+
+    def SetBackgroundImage(self):
+        pixmap = QPixmap('resources/images/welcome_background.png')
+        self.background_label.setPixmap(pixmap.scaled(self.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation))
+        self.background_label.setFixedSize(self.size())
+        self.background_label.lower()  # Ensure the label is behind all other widgets

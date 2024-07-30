19f51b0e65cab9b4ed13132d0c36828d39cd83dd
Liron Tzemach <lirontzemach@gmail.com>
2024-07-30 17:38:44 +0300

    disable the logger when running on executable

--- logger.py
+++ logger.py
@@ -1,12 +1,23 @@
 import logging
+import sys
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)-8s - %(name)-20s - %(message)s',
-    datefmt='%m/%d/%Y %I:%M:%S %p',
-    filename='4Queens.log',
-    filemode='w'
-)
+def setup_logger():
+    if getattr(sys, 'frozen', False):
+        # The application is running as a PyInstaller bundle
+        logging.basicConfig(
+            level=logging.CRITICAL,
+            format='%(asctime)s - %(levelname)-8s - %(name)-20s - %(message)s',
+            datefmt='%m/%d/%Y %I:%M:%S %p'
+        )
+    else:
+        # The application is running in a normal Python environment
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s - %(levelname)-8s - %(name)-20s - %(message)s',
+            datefmt='%m/%d/%Y %I:%M:%S %p',
+            filename='4Queens.log',
+            filemode='w'
+        )
 
 def get_logger(name):
     return logging.getLogger(name)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
+from logger import setup_logger
 from game_manager import GameManager
 
 def main():
+    setup_logger()
     all_queens = GameManager()
     all_queens.load_game()    
     sys.exit()
